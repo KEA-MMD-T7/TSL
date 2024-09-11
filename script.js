7e00ce878e0022998f6425fb210cc5a911a91202
Alan <ale@kea.dk>
2024-09-11 11:51:45 +0200

    brug array.map

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ fetch(url, options)
   .then(bygKatNav);
 
 function bygKatNav(data) {
-  let kats = [];
-  data.forEach((elm) => kats.push(elm.Taksonomi_1));
-  const katOnce = new Set(kats);
-  console.log(katOnce);
+  // let kats = [];
+  // data.forEach((elm) => kats.push(elm.Taksonomi_1));
+  // const katOnce = new Set(kats);
+  const katOnce = new Set(data.map((elm) => elm.Taksonomi_2));
   katOnce.forEach((kat) => {
-    let knap = document.createElement("a");
-    knap.textContent = kat;
-    knap.href = `liste.html?kat=${kat}`;
-    document.querySelector("nav").appendChild(knap);
+    if (kat != "") {
+      let knap = document.createElement("a");
+      knap.textContent = kat;
+      knap.href = `liste.html?kat=${kat}`;
+      document.querySelector("nav").appendChild(knap);
+    }
   });
+  vis(data);
 }
 
 function vis(data) {
